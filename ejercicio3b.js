f52ef6948df3259fd8ba9f58171beb687be4832d
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-20 16:27:10 -0300

    Ejercicio 3b

---
+++ ejercicio3b.js
@@ -0,0 +1,11 @@
+/*
+3.- Al Array del ejercicio anterior utilizarlo para:
+b.- Agregar un elemento al final y al principio del arreglo (métodos push y
+unshift).
+*/
+var frutas = ["naranja","manzana","pera"];
+
+frutas.push("Kiwi");
+frutas.unshift("Frutilla");
+
+alert(frutas);
