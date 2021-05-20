b5e766937bfef04f74f52772cc2bd6f443fa3700
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-20 16:07:44 -0300

    Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+/*
+2.- Crear un Array que contenga al menos 3 nombres de frutas y utilizar el bucle for in
+para recórrelo y mostrar dichos valores con la función console.log
+*/
+
+var frutas = ["naranja","manzana","pera"];
+
+for (i=0; i<frutas.length; i++){
+    console.log(frutas[i])
+}
