f6191ed5f0b4b6cf8279faf0a27fc22997e252b3
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-20 16:16:21 -0300

    Ejercicio 3a

---
+++ ejercicio3a.js
@@ -0,0 +1,8 @@
+/*
+3.- Al Array del ejercicio anterior utilizarlo para:
+a.- Ordenar el array alfabéticamente y mostrar el resultado (método sort).
+*/
+
+var frutas = ["naranja","manzana","pera"];
+
+alert(frutas.sort());
