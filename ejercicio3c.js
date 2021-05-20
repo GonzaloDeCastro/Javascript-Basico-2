84141dbf4167022508631425860376f99987db53
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-20 16:30:47 -0300

    Ejercicio 3c

---
+++ ejercicio3c.js
@@ -0,0 +1,11 @@
+/*
+3.- Al Array del ejercicio anterior utilizarlo para:
+c.- Eliminar un elemento al final y al principio del arreglo (métodos pop y
+shift).
+*/
+var frutas = ["naranja","manzana","pera"];
+
+frutas.pop();
+frutas.shift();
+
+alert(frutas);
