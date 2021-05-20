568f1ff7a42f3b425e6b53f25db27246df0c858a
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-20 16:32:37 -0300

    Ejercicio 3d

---
+++ ejercicio3d.js
@@ -0,0 +1,10 @@
+/*
+3.- Al Array del ejercicio anterior utilizarlo para:
+d.- Invertir el orden de los elementos en el array (método reverse).
+*/
+
+var frutas = ["naranja","manzana","pera"];
+
+frutas.reverse();
+
+alert(frutas);
