07b3f8716904c62f753300a3516fe21f349552b6
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-20 17:11:48 -0300

    Ejercicio 3f

---
+++ ejercicio3f.js
@@ -0,0 +1,12 @@
+/*
+3.- Al Array del ejercicio anterior utilizarlo para:
+f.- Recorrer el array utilizando el bucle for y mostrar cada uno de los valores
+convirtiéndolos a mayúsculas (método toUpperCase de la clase String).
+*/
+
+var frutas = ["naranja","manzana","pera"];
+
+for(i=0; i<frutas.length; i++){
+    alert(frutas[i].toUpperCase());
+}
+
