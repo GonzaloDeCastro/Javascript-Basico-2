09c327fc574a613628ae7e3563e6d0c7c4651567
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-21 17:04:05 -0300

    Ejercicio 4a

---
+++ ejercicio4a.js
@@ -0,0 +1,9 @@
+/*
+4.- Crear una variable que contenga el valor 'La Manzana es Verde' y utilizarlo para:
+a.- Convertirlo a minúscula y mostrar el resultado (método toLowerCase).
+*/
+var palabra;
+
+palabra='La Manzana es Verde';
+
+alert(palabra.toLowerCase());
