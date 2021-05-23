5770e548f29746fea36bb9248b4490f0b237446e
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-23 00:31:42 -0300

    Ejercicio 4c

---
+++ ejercicio4c.js
@@ -0,0 +1,10 @@
+/*
+4.- Crear una variable que contenga el valor 'La Manzana es Verde' y utilizarlo para:
+c.- Obtener los caracteres 3, 4 y 5 del string (método substring).
+*/
+
+var palabra;
+
+palabra='La Manzana es Verde';
+
+alert(palabra.substring(3, 5));
