104622aeab548d8861c6af7a372ff7bba6c2acb2
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-23 00:44:15 -0300

    Ejercicio 4d

---
+++ ejercicio4d.js
@@ -0,0 +1,13 @@
+/*
+4.- Crear una variable que contenga el valor 'La Manzana es Verde' y utilizarlo para:
+d.- Obtener un array con las palabras que hay en el string, asumiendo que cada
+palabra termina cuando se encuentra el carácter espacio (método split).
+*/
+
+var palabra;
+
+palabra='La Manzana es Verde';
+
+arreglo = palabra.split(' ');
+
+alert(arreglo);
