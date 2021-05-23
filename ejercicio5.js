837ee3fa371571d2823c22f00331badf8974927f
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-05-23 15:17:34 -0300

    Ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,10 @@
+/*
+5.- Crear una función que reciba como parámetro un string y que devuelva ese mismo
+string con la primera letra convertida a mayúscula.
+*/
+
+function palabra(a){
+    return a = a.toUpperCase();
+}
+
+alert(palabra('mermelada'));
